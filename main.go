7827e52d9050f1c7c8913fa7908f3287f9e87450
isuzuki <ikkuntech@gmail.com>
2020-09-20 04:06:16 +0900

    added test

--- main.go
+++ main.go
@@ -5,12 +5,17 @@ import (
 	"net/http"
 )
 
-func main() {
+func setupRouter() *gin.Engine {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
 	})
+	return r
+}
+
+func main() {
+	r := setupRouter()
 	r.Run()
 }
